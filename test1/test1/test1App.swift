9e40e0d55f4e36028754c85073a5c35114c3346b
javidebug <cavidgaffar@gmail.com>
2026-01-25 13:31:27 +0400

    Comm1

---
+++ test1/test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Javid Gafarli on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
